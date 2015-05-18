66b068ce40e181b3d18d0641515deb28b3c76250
ttpotato <236852547@qq.com>
2015-05-18 19:07:26 +0800

    initiated

---
+++ libs/image.func.php
@@ -0,0 +1,10 @@
+<?php
+function createVerifyImg(){
+    $len = 4;
+    $img = imagecreatetruecolor(80, 28);
+    $white = imagecolorallocate($img, 255, 255, 255);
+
+    for($i = 0; i < $len; i++){
+        
+    }
+}
